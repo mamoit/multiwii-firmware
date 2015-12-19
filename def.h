de8c0c63f795533f896e5c5ff002cad644b82b3b
Miguel Almeida <migaxmoitax@gmail.com>
2015-12-19 21:40:02 +0000

    Use external resistors instead of the arduino's ones

--- def.h
+++ def.h
@@ -1303,7 +1303,7 @@
   #define ACC_ORIENTATION(X, Y, Z)  {imu.accADC[ROLL]  = -X; imu.accADC[PITCH]  = -Y; imu.accADC[YAW]  =  Z;}
   #define GYRO_ORIENTATION(X, Y, Z) {imu.gyroADC[ROLL] =  Y; imu.gyroADC[PITCH] = -X; imu.gyroADC[YAW] = -Z;}
   #define MAG_ORIENTATION(X, Y, Z)  {imu.magADC[ROLL]  =  X; imu.magADC[PITCH]  =  Y; imu.magADC[YAW]  = -Z;}
-  //#undef INTERNAL_I2C_PULLUPS
+  #undef INTERNAL_I2C_PULLUPS
   #define ADXL345_ADDRESS 0x53
 #endif
 
